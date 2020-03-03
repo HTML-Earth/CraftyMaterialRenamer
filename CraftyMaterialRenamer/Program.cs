8cf19b668bd4556af8b6c1e72c673608a32c99c3
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2020-03-03 20:28:10 +0100

    fix texture names with "maps/" at the beginning

--- CraftyMaterialRenamer/Program.cs
+++ CraftyMaterialRenamer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace CraftyMaterialRenamer
 {
@@ -45,9 +46,24 @@ namespace CraftyMaterialRenamer
                     
                     if (lines[i].StartsWith("#"))
                     {
-                        realTextureName = lines[i].Substring(2, lines[i].Length - 2).ToLower();
-                        //if (realTextureName.StartsWith("maps/"))
-                            // fix it
+                        // Cut off "# "
+                        realTextureName = lines[i].Substring(2, lines[i].Length - 2);
+
+                        // Fix texture names that look like "maps/d1_canals_02/stone/stonewall051b_-477_1583_-795"
+                        if (realTextureName.StartsWith("maps/"))
+                        {
+                            Regex rx = new Regex(@"(?:^maps/[0-z]+/)(.*)(?:(_-?[0-9]+){3}$)", RegexOptions.Compiled);
+                            MatchCollection matches = rx.Matches(realTextureName);
+
+                            if (matches.Count == 1)
+                                realTextureName = matches[0].Result("$1");
+                            else
+                                Console.WriteLine($"Regex error: Found {matches.Count} matches for {realTextureName}");
+                        }
+                        else // Lowercase for texture names that look like "CONCRETE/CONCRETEWALL071E"
+                        {
+                            realTextureName = lines[i].ToLower();
+                        }
                     }
                 }
                 
